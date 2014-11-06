3029feeb464be495152b5a72c2aa55dc4095e304
lane <zhanglintc623@gmail.com>
2014-11-06 17:39:07 +0800

    bug fix!!!
    %1 -> %~1, see comment in the file

--- src/wb.cmd
+++ src/wb.cmd
@@ -8,7 +8,11 @@ set parameter=
 
 REM concatenate all input parameters
 :loop
-if "%1" neq "" set parameter=%parameter% %1 & shift & goto loop
+if "%~1" neq "" set parameter=%parameter% %1 & shift & goto loop
+REM (%1) -> (%~1), strip the quotation mark
+REM see if (%1) is ("a b"), thus ("%1") is (""a b"") means ("") and (a b) and ("")
+REM change (%1) to (%~1) means ("a b") become (a b), then ("%1") means ("a b")
+REM well, it's ok now
 
 REM call main Python file
 python xiaobawang.py %parameter%
