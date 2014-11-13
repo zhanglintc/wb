7729b7cb802495979122647e4630593811454fb9
lane <zhanglintc623@gmail.com>
2014-11-13 16:32:05 +0800

    uninstall

---
+++ linux/uninstall.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, shutil
+
+# /Users/lane
+gen_path  = os.path.expanduser('~') # your home path
+
+# /Users/lane/.bash_profile
+bash_path = "{}/.bashrc".format(gen_path) # your .bash_profile path
+
+# /Users/lane/temp
+temp_path = "{}/temp".format(gen_path) # temp file path
+
+fr = open(bash_path, 'r')
+fw = open(temp_path, 'w')
+
+line = True; install_command = False
+while line:
+    line = fr.readline()
+    if 'wb.py' not in line: # if this line do not contain wb.py, write it to file
+        fw.write(line)
+    else: # else jump it, but store to print
+        install_command = line
+
+fr.close()
+fw.close()
+
+shutil.copy(temp_path, bash_path) # replace .bash_profile by temp file
+os.remove(temp_path) # delete temp file
+
+if install_command:
+    print("The command below:\n\n  {}\n\nis successfully removed from .bash_profile\n".format(install_command[:-1]))
+else:
+    print("Well, there is nothing to delete\n")
+
+try:
+    raw_input()
+except:
+    pass
+
+
