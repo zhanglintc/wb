ebc2ed1f5a408ee4f4a165223e5c9bec1df9d35a
Lane <zhanglintc623@gmail.com>
2014-11-17 18:20:45 +0800

    general install & uninstall

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import platform
+import os
+
+if 'Linux' in platform.platform():
+    print('Linux detected\n')
+    os.system('cd ./linux & python install.py')
+
+if 'Darvin' in platform.platform():
+    print('Mac detected\n')
+    os.system('cd ./mac & python install.py')
+
+if 'Windows' in platform.platform():
+    print('Windows detected\n')
+    os.system('cd ./win & python install.py')
---
+++ uninstall.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import platform
+import os
+
+if 'Linux' in platform.platform():
+    print('Linux detected\n')
+    os.system('cd ./linux & python uninstall.py')
+
+if 'Darvin' in platform.platform():
+    print('Mac detected\n')
+    os.system('cd ./mac & python uninstall.py')
+
+if 'Windows' in platform.platform():
+    print('Windows detected\n')
+    os.system('cd ./win & python uninstall.py')
