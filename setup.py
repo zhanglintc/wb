59409104c9df0d2dbbf417df4101da5faeeb7a8d
lane <zhanglintc623@gmail.com>
2014-11-17 20:36:33 +0800

    gen install

--- setup.py
+++ setup.py
@@ -4,14 +4,45 @@
 import platform
 import os
 
+print('')
+print("This tool helps you add 'wb' command to your device")
+print("It's going to detect your device automatically")
+print("If faild or detection not right, please do it manually\n")
+
+choice = None
 if 'Linux' in platform.platform():
-    print('Linux detected\n')
-    os.system('cd ./linux && python install.py')
+    while choice != 'y' and choice != 'n':
+        choice = raw_input("'Linux' detected, continue?[y/n]").lower()
 
-if 'Darwin' in platform.platform():
-    print('Mac detected\n')
-    os.system('cd ./mac && python install.py')
+    if choice == 'y':
+        os.system('cd ./linux && python install.py')
+    else:
+        print('')
+        print("setup aborted\n")
 
-if 'Windows' in platform.platform():
+elif 'Darwin' in platform.platform():
+    while choice != 'y' and choice != 'n':
+        choice = raw_input("'Mac' detected, continue?[y/n]").lower()
+
+    if choice == 'y':
+        os.system('cd ./mac && python install.py')
+    else:
+        print('')
+        print("setup aborted\n")
+
+elif 'Windows' in platform.platform():
     print('Windows detected\n')
-    os.system('cd ./win & python install.py')
+    while choice != 'y' and choice != 'n':
+        choice = raw_input("'Windows' detected, continue?[y/n]").lower()
+
+    if choice == 'y':
+        os.system('cd ./win & python install.py')
+    else:
+        print('')
+        print("setup aborted\n")
+
+else:
+    print("Can't detect your device, please do it by yourself\n")
+
+
+
