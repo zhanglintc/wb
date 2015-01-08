3b41f98c7d29eb35c07a926cdb8046049477c374
Lane <zhanglintc623@gmail.com>
2015-01-08 16:40:28 +0800

    os_type modification

--- setup.py
+++ setup.py
@@ -20,22 +20,24 @@ print("If faild or detection not right, please do it manually\n")
 choice = None
 os_info = platform.platform()
 
+
+# os_type[1]: for folder use
 if 'Linux' in os_info:
-    os_type = 'Linux'
+    os_type = ['Linux', 'linux']
 elif 'Darwin' in os_info:
-    os_type = 'Mac'
+    os_type = ['Mac', 'mac']
 elif 'Windows' in os_info:
-    os_type = 'Windows'
+    os_type = ['Windows', 'win']
 else:
     print("Can't detect your device, please do it by yourself\n")
     raw_input()
     sys.exit(0)
 
 while choice != 'y' and choice != 'n':
-    choice = raw_input("'{}' detected, continue?[y/n]".format(os_type)).lower()
+    choice = raw_input("'{}' detected, continue?[y/n]".format(os_type[0])).lower() # os_type[0]: for display
 
     if choice == 'y':
-        os.system('cd ./{} && python install.py'.format(os_type[:3].lower()))
+        os.system('cd ./{} && python install.py'.format(os_type[1])) # os_type[1]: for folder use
     else:
         print('')
         print("setup aborted\n")
